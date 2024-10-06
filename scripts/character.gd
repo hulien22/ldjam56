93c26fd983f4cdada6c1522be145565c5e6e750d
Eimh <41556634+Eimh@users.noreply.github.com>
2024-10-06 15:34:40 -0700

    animals & teams

--- scripts/character.gd
+++ scripts/character.gd
@@ -7,3 +7,6 @@ extends Resource
 @export var model: PackedScene
 @export var chance: int
 @export var nationality: String
+@export var type: Type
+
+enum Type { ANT, BAT, CHEETAH, DUCK, FROG, GIRAFFE, GOAT, GRASSHOPPER, KANGAROO, PENGUIN, RAT, TURTLE }
---
+++ scripts/team.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var GK: Character
+var ST: Character
+var LM: Character
+var RM: Character
