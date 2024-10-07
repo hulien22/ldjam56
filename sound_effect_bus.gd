4119c1139eecc758a21b53aecd3163209db06492
Eimh <41556634+Eimh@users.noreply.github.com>
2024-10-06 20:35:54 -0700

    sound effects

---
+++ sound_effect_bus.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer
+
+@export var beep_low: AudioStream
+@export var beep_high: AudioStream
+@export var pop: AudioStream
+@export var vending: AudioStream
+
+func play_countdown_beep():
+	stream = beep_low
+	play()
+	
+func play_final_beep():
+	stream = beep_high
+	play()
+
+func play_pop():
+	stream = pop
+	play()
+	
+func play_vending():
+	stream = vending
+	play()
