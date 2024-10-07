ff4dae35a9aea7109346a3d38018fba96a83a76b
hulien22 <hejulien22@gmail.com>
2024-10-07 01:10:18 -0700

    diff textures:

--- character_option.gd
+++ character_option.gd
@@ -2,9 +2,20 @@ extends Control
 
 signal selected_character(character: Character)
 
+@export var text_b: CompressedTexture2D
+@export var text_a: CompressedTexture2D
+@export var text_s: CompressedTexture2D
+
 var char: Character
 
 func set_data(data: Character):
+	match data.chance:
+		2:
+			$TextureButton.texture_normal = text_b
+		1:
+			$TextureButton.texture_normal = text_a
+		_:
+			$TextureButton.texture_normal = text_s
 	char = data
 	$TextureButton/Label.text = char.name
 
